1c3393bbf6a06918e4067f30eefa573e1a5531c8
Brett Spangler <bns5273@psu.edu>
2018-09-11 16:24:18 -0400

    new graphs

--- setons_nn.py
+++ setons_nn.py
@@ -4,7 +4,7 @@ import json
 import urllib.request
 import plotly.graph_objs as go
 import plotly.plotly as py
-import torch
+import torch as torch
 import torch.nn as nn
 from math import sqrt
 from trueskill import BETA  # == 4.1666_
@@ -119,7 +119,7 @@ for players in fullGames:
             x[0][f][i] = m
             x[1][f][i] = d
 
-        matches.append(torch.Tensor(x))
+        matches.append(torch.FloatTensor(x))
         results.append(r)
         estimates.append(cdf(trueskill(match)))
 
@@ -186,13 +186,22 @@ for j in range(epochs):
 # for param in net.parameters():
 #     print(param.data)
 
-neuralvstrueskill = [
-    go.Scatter(
+vsTrueskill = [
+    go.Histogram(
+        name='TrueSkill',
         x=testing_trueskill,
-        y=predictions,
-        mode='markers'
+        opacity=.75
+    ),
+    go.Histogram(
+        name='Neural Network',
+        x=predictions,
+        opacity=.75
     )
 ]
+layout = go.Layout(barmode='overlay')
+fig = go.Figure(data=vsTrueskill, layout=layout)
+py.plot(fig, filename='neuralvstrueskill')
+
 netvtime = [
     go.Scatter(
         name='percentage',
@@ -219,7 +228,6 @@ netvtime = [
         mode='lines'
     )
 ]
-py.plot(neuralvstrueskill, filename='neuralvstrueskill')
 py.plot(netvtime, filename='netvtime')
 
 
